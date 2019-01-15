4f428c6db1d30eebb2acc6ef7cc32711b450629a
ThuyNT13 <ngo.thuy38@gmail.com>
2019-01-15 15:04:13 -0800

    Remove unneccessary code

--- cipherCaesar.cpp
+++ cipherCaesar.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// string convertAsciiToChar(char ascii, int indexAlphaLower, int indexAlphaUpper, string cipherText)
 void convertAsciiToChar(char ascii, int indexAlphaLower, int indexAlphaUpper, string& cipherText)
 {
 
